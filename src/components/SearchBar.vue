273e249fad7f3355cbd22ba8e0ce763b092dd3d3
SpyrosPapazisis <spyrospapazisis88@gmail.com>
2020-01-12 23:01:51 +0000

    upload on github

---
+++ src/components/SearchBar.vue
@@ -0,0 +1,27 @@
+<template>
+    <div>
+        <input @input="onInput" placeholder="Search..."/>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'SearchBar',
+    methods: {
+        onInput(event) {
+            //console.log(event.target.value);
+            this.$emit('termChange', event.target.value)
+        }
+    }
+};
+</script>
+
+<style scoped>
+    input {
+        width: 75%;
+    }
+    div {
+        text-align: center;
+        margin: 20px;
+    }
+</style>
